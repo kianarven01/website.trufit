379b2bcd2a2b20e6d91dc3a7c8556dc106be269b
Kian Arven Valencia <arvenkian1234@gmail.com>
2026-03-01 14:58:11 +0800

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,6 +10,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/verify-registration-key', [AuthController::class, 'verifyKey']);
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::get('/test', function() {
+    return response()->json(['message' => 'Backend is reached!']);
+});
+
 
 //for admin role management CRUD
 Route::get('/admin/roles', [RoleController::class, 'index']);

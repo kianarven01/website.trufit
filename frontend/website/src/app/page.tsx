24d616841b6cbb37c3e6f5ad057900e1ba4ebf5c
rin <pjdsena@gmail.com>
2026-03-13 17:19:50 +0800

    homepage service section

--- frontend/website/src/app/page.tsx
+++ frontend/website/src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/home/hero/herosection"
+import ServicesSection from "@/components/home/services/servicessection"
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <ServicesSection/>
     </>
   )
 }

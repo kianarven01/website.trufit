5cc967283a8feaba0bcf9d2d2fbdcb456ac3e16a
Kian Arven Valencia <arvenkian1234@gmail.com>
2026-03-14 10:17:31 +0800

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -34,6 +34,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];

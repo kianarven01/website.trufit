81aafadc1851e3ee29bcc01f14c74d04b0e67a8f
ReiAkagiLouverne28 <arvenkian1234@gmail.com>
2026-04-11 11:34:52 +0800

    Update sitemap.ts

--- frontend/website/src/app/sitemap.ts
+++ frontend/website/src/app/sitemap.ts
@@ -29,12 +29,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
-    // {
-    //   url: `${baseUrl}/news`,
-    //   lastModified: new Date(),
-    //   changeFrequency: 'weekly',
-    //   priority: 0.5,
-    // },
+    /*{
+      url: `${baseUrl}/news`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },*/
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),

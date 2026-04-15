c25a4e0114dd8ff0aa97e5be5f9ead061caf79b6
rin <pjdsena@gmail.com>
2026-04-15 18:04:30 +0800

    Update slide.ts

--- frontend/website/src/data/slides.ts
+++ frontend/website/src/data/slides.ts
@@ -2,7 +2,7 @@ import { Slide } from "@/types/slide"
 
 export const slides: Slide[] = [
   {
-    image: "/images/about/accreditation_real/suzuki.jpg",
+    image: "/images/about/accreditation_real/suzuki.webp",
     smallHeader: "Official Service Center",
     title: "Authorized Suzuki Service Center",
     subtitle: "We are an authorized Suzuki Service Station here at Daet. We follow Suzuki’s Standard Quality Services to keep you safe on the road.",
@@ -10,7 +10,7 @@ export const slides: Slide[] = [
     secondaryLink: "/about",
   },
   {
-    image: "/images/home/different_cars.jpg",
+    image: "/images/home/different_cars.webp",
     smallHeader: "The TRUFIT standard",
     title: "Great Care for All Cars",
     subtitle: "From oil change to engine fix, we take good care of every car that comes through our shop. We use the best parts and the best service to keep your car in a good shape.",
@@ -18,7 +18,7 @@ export const slides: Slide[] = [
     secondaryLink: "/",
   },
   {
-    image: "/images/home/complete_tools.JPG",
+    image: "/images/home/complete_tools.webp",
     smallHeader: "Complete set of Tools",
     title: "The Right Tools for any Job",
     subtitle: "Our shop is complete with special tools for any kind of fix. We can find the problem faster and fix it better.",
@@ -26,7 +26,7 @@ export const slides: Slide[] = [
     secondaryLink: "/",
   },
   {
-    image: "/images/home/dti_accreditation.jpg",
+    image: "/images/home/dti_accreditation.webp",
     smallHeader: "DTI 5-star Accredited Workshop",
     title: "Service You Can Trust",
     subtitle: "Our service is backed by DTI’s Bagwis Seal of excellence, proving the quality and reliability of our services.",

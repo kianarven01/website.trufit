14f7688f5e8718e5819cfa8d033b257b9da3fd08
rin <pjdsena@gmail.com>
2026-04-21 17:41:08 +0800

    Prevent Right Click on Service Video

--- frontend/website/src/components/services/ServiceVideo.tsx
+++ frontend/website/src/components/services/ServiceVideo.tsx
@@ -58,6 +58,7 @@ export default function ServiceVideo() {
               controlsList="nodownload"
               playsInline
               preload="metadata"
+              onContextMenu={(e) => e.preventDefault()}
             />
           </div>
 

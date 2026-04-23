1216bd7a954af08ff85b55386d0cad92a61d267f
rin <pjdsena@gmail.com>
2026-04-23 16:39:39 +0800

    Change Hamburger Font to Barlow

--- frontend/website/src/components/layout/navbar/navbar.tsx
+++ frontend/website/src/components/layout/navbar/navbar.tsx
@@ -222,7 +222,7 @@ export default function Navbar() {
                   <Link
                     key={link.name}
                     href={link.href}
-                    className="text-white text-3xl font-brawler hover:text-brand-red transition-colors"
+                    className="text-white text-3xl hover:text-brand-red transition-colors"
                     onClick={() => setIsOpen(false)}
                   >
                     {link.name}

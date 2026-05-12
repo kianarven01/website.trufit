2032243358ef129d927d906e8eac3152bc1b9d9e
CafePol <119393703+CfPol@users.noreply.github.com>
2026-05-13 07:16:58 +0800

    refactor(api): remove unused product reference routes for cleaner structure

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,31 +9,15 @@ use App\Domains\KeyManagement\Http\Controllers\KeyController;
 // Product + Reference routes
 require app_path('Domains/Product/routes.php');
 
+// Vehicle routes
+require app_path('Domains/Vehicle/routes.php');
+
 /*
 |--------------------------------------------------------------------------
 | Supplier Reference Routes
 |--------------------------------------------------------------------------
 */
 
-
-/*
-|--------------------------------------------------------------------------
-| Product Reference Routes
-|--------------------------------------------------------------------------
-*/
-
-Route::prefix('products')->group(function () {
-
-    Route::get('/', [ProductController::class, 'index']);
-
-    Route::get('/categories', [ProductReferenceController::class, 'categories']);
-    Route::get('/units', [ProductReferenceController::class, 'units']);
-    Route::get('/suppliers', [SupplierController::class, 'index']);
-    Route::get('/vehicles', [ProductReferenceController::class, 'vehicles']);
-    Route::post('/vehicles/custom', [ProductReferenceController::class, 'storeCustomVehicle']);
-    Route::get('/manufacturers', [ProductReferenceController::class, 'manufacturers']);
-    Route::get('/service-types', [ProductReferenceController::class, 'serviceTypes']);
-});
 /*
 |--------------------------------------------------------------------------
 | Authentication Domain
